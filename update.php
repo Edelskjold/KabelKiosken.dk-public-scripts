707d4674065ca83b1b261d712c3868234add4c0e
Mads K. Edelskjold <Edelskjold@users.noreply.github.com>
2017-03-02 15:03:17 +0100

    Update update.php

--- update.php
+++ update.php
@@ -2,7 +2,7 @@
 /**
  * The following End User License Agreement ("EULA") in the program/file represents the contractual conditions between you ("Licensee") and the author listed in this document @author for the use of software including related media, documentation (for example program descriptions, manuals) and other documents and materials. By installing and- or using the software on your computer/server, you declare that you have a written consent from the @author to use the associated files. If you do not have a written consent, you must not install the software. If you are installing the software you automatically accepts the license agreement.
  *
- * @file 	  import.php
+ * @file      update.php
  * @package   KabelKiosken-Helper
  * @version   1.0.0
  * @author    Mads K. Edelskjold
